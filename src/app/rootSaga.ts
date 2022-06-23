2c747c3769b3a91c5c6cabf5490f7bdcea5c8115
Dat Truong <dat.truong@propzy.com>
2022-06-23 12:03:20 +0700

    1. react-router-dom v5, 2. setup layout and private route

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,14 +1,12 @@
 import { all } from 'redux-saga/effects';
 
-import counterSaga from 'features/counter/counterSaga';
-
 function* helloSaga() {
   console.log('hello');
 }
 
 function* rootSaga() {
   console.log('Root saga');
-  yield all([helloSaga(), counterSaga()]);
+  yield all([helloSaga()]);
 }
 
 export default rootSaga;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import counterReducer from '../features/counter/counterSlice';
 import rootSaga from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
 });
 
